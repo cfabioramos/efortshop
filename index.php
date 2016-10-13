2a74acca6b12c053b00018c8f72ebd64e04b1001
Carlos Fábio Ramos Conceição <cfabioramos@gmail.com>
2016-10-13 12:44:31 -0300

    Ajustes nos depoimentos.

--- index.php
+++ index.php
@@ -430,30 +430,6 @@ Ah! No nome, a palavra Efort vem da palavra Effort (esforço em inglês). Apenas r
 
 					<div id="testimonial" class=" wow animated fadeInUp">
 						<div class="testimonial-item text-center">
-							<img src="img/member-1.jpg" alt="Our Clients">
-							<div class="clearfix">
-								<span>Katty Flower</span>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-									sed do eiusmod tempor incididunt ut labore et dolore magna
-									aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-									ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-									aute irure dolor in reprehenderit in voluptate velit esse
-									cillum dolore eu fugiat nulla pariatur.</p>
-							</div>
-						</div>
-						<div class="testimonial-item text-center">
-							<img src="img/member-1.jpg" alt="Our Clients">
-							<div class="clearfix">
-								<span>Katty Flower</span>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-									sed do eiusmod tempor incididunt ut labore et dolore magna
-									aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-									ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-									aute irure dolor in reprehenderit in voluptate velit esse
-									cillum dolore eu fugiat nulla pariatur.</p>
-							</div>
-						</div>
-						<div class="testimonial-item text-center">
 							<img src="img/testimonials/priscila.jpg" alt="Nossos clientes">
 							<div class="clearfix">
 								<span>Priscila Carvalho</span>
@@ -464,6 +440,18 @@ Ah! No nome, a palavra Efort vem da palavra Effort (esforço em inglês). Apenas r
 								executam os trabalhos com qualidade.</p>
 							</div>
 						</div>
+						<!--div class="testimonial-item text-center">
+							<img src="img/member-1.jpg" alt="Our Clients">
+							<div class="clearfix">
+								<span>Katty Flower</span>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+									sed do eiusmod tempor incididunt ut labore et dolore magna
+									aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+									ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+									aute irure dolor in reprehenderit in voluptate velit esse
+									cillum dolore eu fugiat nulla pariatur.</p>
+							</div>
+						</div-->
 					</div>
 
 				</div>
